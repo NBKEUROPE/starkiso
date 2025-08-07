d6a7a332eb0437a4fb87d8c5136c7a14e887d5f2
NBKEUROPE <nbkwallet@gmail.com>
2025-08-07 15:51:05 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
+# app/__init__.py
 
+from flask import Flask
+from .routes import app as flask_app  # Correct relative import for the Blueprint
+
+def create_app():
+    # Create the Flask application instance
+    app = Flask(__name__)
+    # Set a secret key for session management
+    app.secret_key = 'blackrock-secret'
+
+    # Register the Blueprint from app/routes.py
+    app.register_blueprint(flask_app)
+    
+    return app
