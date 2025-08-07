3f881bf0b316a6e82e172cecc85cf5028038e0bd
NBKEUROPE <nbkwallet@gmail.com>
2025-08-07 15:53:51 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
-# app/__init__.py
-
+# app.py
 from flask import Flask
-from .routes import app as flask_app  # Correct relative import for the Blueprint
+from iso_server.server import start_iso_server
+from app.routes import app as flask_app
 
-def create_app():
-    # Create the Flask application instance
-    app = Flask(__name__)
-    # Set a secret key for session management
-    app.secret_key = 'blackrock-secret'
+# Create the main Flask application instance
+app = Flask(__name__)
+# Set a secret key for session management
+app.secret_key = 'blackrock-secret'
 
-    # Register the Blueprint from app/routes.py
-    app.register_blueprint(flask_app)
-    
-    return app
+# Register the Blueprint from app/routes.py
+app.register_blueprint(flask_app)
+
+if __name__ == '__main__':
+    # This block is for local development only.
+    # Gunicorn will not run this part.
+    start_iso_server()
+    app.run(debug=True, host='0.0.0.0', port=5000)
